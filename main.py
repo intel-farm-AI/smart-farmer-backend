96591c09328bc82da5b464028effa9da3f6f9224
Muhammad Sultan <sultanurulloh08@gmail.com>
2026-02-23 17:08:43 +0700

    fix: backend bug fix

--- main.py
+++ main.py
@@ -22,29 +22,27 @@ app.add_middleware(
 
 # === Load Model OpenVINO ===
 ie = Core()
-model = ie.read_model("models/model-v3/plant-disease-model-v3.xml")
+model = ie.read_model("models/model-v3.1/plant-disease-v3.1.xml")
 compiled_model = ie.compile_model(model=model, device_name="CPU")
 input_layer = compiled_model.input(0)
 output_layer = compiled_model.output(0)
 
 # === Labels ===
-with open("models/model-v3/labels_id.json", encoding="utf-8") as f:
+with open("models/model-v3.1/labels.json", encoding="utf-8") as f:
     class_indices = json.load(f)
 
-# Buat list kosong dengan panjang sesuai jumlah kelas
+# Buat list berdasarkan index angka
 class_names = [""] * len(class_indices)
 
 for key, name in class_indices.items():
-    # Ambil angka terakhir dari key (setelah underscore "_")
-    # contoh: "PlantVillage_0" -> 0
-    idx = int(key.split("_")[-1])
+    idx = int(key)
     class_names[idx] = name
 
 
 
 # === Dataset Penyakit ===
 disease_info_map = {}
-with open("models/model-v3/disease_label.json", encoding="utf-8") as f:
+with open("models/model-v3.1/disease_label.json", encoding="utf-8") as f:
     disease_data = json.load(f)
     for item in disease_data:
         disease_info_map[item['nama_penyakit']] = {
@@ -92,4 +90,4 @@ def predict(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
